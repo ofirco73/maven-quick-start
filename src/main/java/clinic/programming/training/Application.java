c0563b550b1405813851dce3d7b927a17f69757e
Ofir Cohen <ofirc@il.ibm.com>
2018-02-11 16:48:50 +0200

    dependency

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,6 +1,24 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
+    
+    public void greet() {
+        List<String> greetings = new ArrayList<>();
+        greetings.add("Hello");
+        
+        for(String greeting : greetings) {
+            System.out.println("Greeting: " + greeting);
+        }
+    }
+    
+    public int countWords(String words) {
+        String[] separateWords = StringUtils.split(words, ' ');
+        return (separateWords == null) ? 0 : separateWords.length;
+    }
     
     public Application() {
         System.out.println ("Inside Application");
@@ -9,6 +27,9 @@ public class Application {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	Application app = new Application();
+        Application app = new Application();
+        app.greet();
+        int count = app.countWords("I have four words");
+        System.out.println("Word Count: " + count);
     }
 }
